b719494f3128d6bd6669cb149cdf5d872467c7a2
Avalanching <18269285645@163.com>
2017-03-26 16:52:26 +0800

    Initial Commit

---
+++ QYJSomeAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYJSomeAnimation
+//
+//  Created by Avalanching on 2017/3/26.
+//  Copyright © 2017年 Avalanching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYJSomeAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYJSomeAnimation
+//
+//  Created by Avalanching on 2017/3/26.
+//  Copyright © 2017年 Avalanching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
